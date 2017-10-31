98b79f5efbb7d6a1a27faaf3fcb83c4d609cbfb2
allisonbmccoy <amccoyphd@gmail.com>
2017-10-31 09:27:14 -0500

    Fixed for new layout and revert to single stats file

--- scrape.js
+++ scrape.js
@@ -4,71 +4,68 @@ var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
 var people = require(process.argv[2]);
-var n = (process.argv[2]).split("\.")[1].split("-")[2];
 
 var scrapeEntry = function(person, doneCallback) {
   var url = people[person];
   var data = {};
 
-  request(url, function(err, resp, body) {
-    $ = cheerio.load(body);
+  // properly set the encoding, or we'll mangle accented characters:
+  // http://stackoverflow.com/questions/8332500/module-request-how-to-properly-retrieve-accented-characters-%EF%BF%BD-%EF%BF%BD-%EF%BF%BD
+  request({ encoding: 'binary', method: "GET", uri: url}, function(err, resp, body) {
+    var $ = cheerio.load(body);
 
     try {
-      //var affiliation = $('.gsc_prf_il', '#gsc_prf_i')[0].children[0].data;
+      // We're output to stdout, so log to stderr
+      console.error("Scraping " + person + "...");
+
+      // var photo = $('#gsc_prf_pup-img')[0].attribs.src;
       var affiliation = $('.gsc_prf_il', '#gsc_prf_i').first().text();
 
+      // var keywords_root = $('#gsc_prf_int')[0].children;
+      // var keywords = [];
+
+      // for (var i=0; i<keywords_root.length; i++) {
+      //  keywords.push(keywords_root[i].children[0].data);
+      //}
+
       var rawStats = $('#gsc_rsb_st');
+
       var stats = {
-        'citations' : [ rawStats[0].children[1].children[1].children[0].data,
-                        rawStats[0].children[1].children[2].children[0].data],
-        'hindex' : [ rawStats[0].children[2].children[1].children[0].data,
-                     rawStats[0].children[2].children[2].children[0].data],
-        'i10index' : [ rawStats[0].children[3].children[1].children[0].data,
-                       rawStats[0].children[3].children[2].children[0].data]
+        'citations' : [ rawStats[0].children[1].children[0].children[1].children[0].data,
+                        rawStats[0].children[1].children[0].children[2].children[0].data ],
+        'hindex'    : [ rawStats[0].children[1].children[1].children[1].children[0].data,
+                        rawStats[0].children[1].children[1].children[2].children[0].data ],
+        'i10index'  : [ rawStats[0].children[1].children[2].children[1].children[0].data,
+                        rawStats[0].children[1].children[2].children[2].children[0].data ]
       };
+
+      var rawYear = $('.gsc_md_hist_b');
 
       data = {
         'name' : person,
-        'affiliation' : affiliation,
         'url': url,
-        'stats' : stats
+        //'photo' : 'http://scholar.google.com' + photo,
+        'affiliation' : affiliation,
+        //'keywords' : keywords,
+        'stats' : stats,
+        'year' : rawYear[0].children[0].children[0].data
       };
 
     } catch (ex) {
+      console.error(ex);
       throw new Error(person);
     }
 
-    doneCallback(null, data);
-  });
-};
-
-var scrapeYear = function(entry, doneCallback) {
-  var histUrl = entry['url'] + "&view_op=citations_histogram";
-
-  request(histUrl, function(err, resp, body) {
-    $ = cheerio.load(body);
-
-    try {
-      year = $('.gsc_g_t')[0].children[0].data;
-    } catch(ex) {
-      year = new Date().getFullYear();
-    }
-
-    entry['year'] = year;
-
-    doneCallback(null, entry);
+    // Adding a timeout to regulate scraping speed.
+    setTimeout(function() {
+      doneCallback(null, data);
+    }, 1000);
   });
 };
 
 // http://javascriptplayground.com/blog/2013/06/think-async/
 async.mapSeries(Object.keys(people), scrapeEntry, function (err, results) {
-  async.mapSeries(results, scrapeYear, function (err, results) {
-    var date = new Date();
-    console.log('date_array[' + n + '] = "' + date + '";');
-    if (n) {
-	    console.log('data_array[' + n + '] = ' + JSON.stringify(results, null, 2) + ';');
-    } else {
-	console.log('data = ' + JSON.stringify(results, null, 2) + ';');
-    }
-  });
+  var date = new Date();
+  console.log('var date = "' + date + '"');
+  console.log('var data = ' + JSON.stringify(results, null, 2) + ';');
 });
